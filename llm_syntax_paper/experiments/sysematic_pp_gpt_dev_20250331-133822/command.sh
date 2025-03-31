0d4096c77311173e70d48b401004e3b907913111
Greg <greg@coppola.ai>
2025-03-31 13:39:12 -0500

    working

---
+++ llm_syntax_paper/experiments/sysematic_pp_gpt_dev_20250331-133822/command.sh
@@ -0,0 +1,1 @@
+python python/systematic_pp/gptapi_against_gpt.py data/input/systematic_pp/chatgpt_generated_20.json --live_run --output_base data/output/systematic_pp/chatgpt_generated_20.gptapi.json
