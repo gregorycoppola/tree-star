dc5e0b8fa1cb31ba8cd036fffbe281fbe92916b1
Greg <greg@coppola.ai>
2025-03-31 20:14:57 -0500

    working

---
+++ llm_syntax_paper/experiments/pp_stanza_heldout2_20250331-201358/command.sh
@@ -0,0 +1,1 @@
+python python/systematic_pp/stanza_against_gpt.py data/input/systematic_pp/chatgpt_generated_20.heldout2.json --live_run --output_file data/output/systematic_pp/chatgpt_generated_20.heldout2.stanza.conllu
