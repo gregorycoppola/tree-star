fbd16001430688212b4a9fb8e5e2a9510c7fc163
Greg <greg@coppola.ai>
2025-03-31 20:06:37 -0500

    working

---
+++ llm_syntax_paper/experiments/pp_gptapi_heldout1_20250331-200603/command.sh
@@ -0,0 +1,1 @@
+python python/systematic_pp/gptapi_against_gpt.py data/input/systematic_pp/chatgpt_generated_20.heldout1.json --live_run --output_base data/output/systematic_pp/chatgpt_generated_20.heldout1.gptapi.json
